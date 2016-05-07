c5cbdd133969c4a96fb976bcc562ca8bbce752c0
Bernhard Häussner <bxt@die-optimisten.net>
2016-05-07 23:30:05 +0200

    Make watch.sh take a filename as parameter

--- watch.sh
+++ watch.sh
@@ -1,10 +1,15 @@
-fswatch -0 index.rhtml | while read -d "" event
+INFILE=$1
+OUTFILE=${INFILE%.rhtml}.html
+
+echo $INFILE to $OUTFILE
+
+fswatch -0 $INFILE | while read -d "" event
 do
   echo -n "Building... "
-  erb index.rhtml &> index.html
+  erb $INFILE &> $OUTFILE
   if [ $? -ne 0 ]; then
     sed -i '' -e '1i\
-    <pre style="background:#fdd;">' index.html
+    <pre style="background:#fdd;">' $OUTFILE
   fi
   echo "done."
 done
